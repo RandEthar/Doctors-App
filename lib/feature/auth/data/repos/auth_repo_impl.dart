a078659047fa826abb89d452749dd416553cf8cf
randethar <randethar@gmail.com>
2025-09-14 10:12:24 +0100

    create auth repo imple

---
+++ lib/feature/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:doctors_app/core/errors/failure.dart';
+import 'package:doctors_app/core/services/api_constants.dart';
+import 'package:doctors_app/core/services/api_service.dart';
+import 'package:doctors_app/feature/auth/data/models/login_request_body.dart';
+import 'package:doctors_app/feature/auth/data/models/login_response.dart';
+import 'package:doctors_app/feature/auth/domain/repos/auth_repo.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  final ApiService apiService;
+
+  AuthRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failure, LoginResponse>> signInWithEmailAndPassword(
+      LoginRequestBody loginRequestBody) async {
+    try {
+      final data = await apiService.post(
+        endPoint: ApiConstants.login,
+        data: loginRequestBody.toJson(),
+      );
+      return Right(LoginResponse.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
+
