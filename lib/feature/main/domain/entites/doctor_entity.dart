8d2e8c009f2581f01394fe0fe907d2b5106d83ec
randethar <randethar@gmail.com>
2025-09-17 19:22:10 +0100

    generate doctor entity

--- lib/feature/main/domain/entites/doctor_entity.dart
+++ lib/feature/main/domain/entites/doctor_entity.dart
@@ -1,11 +1,26 @@
+import 'package:hive/hive.dart';
+part 'doctor_entity.g.dart';
+
+@HiveType(typeId: 0)
 class DoctorEntity {
+  @HiveField(0)
   final int? id;
-  final String ?name;
-  final String ?specialty; 
-  final String ?imageUrl;
-  final String ?startTime;
-  final String ?endTime;
+  @HiveField(1)
+  final String? name;
+  @HiveField(2)
+  final String? specialty;
+  @HiveField(3)
+  final String? imageUrl;
+  @HiveField(4)
+  final String? startTime;
+  @HiveField(5)
+  final String? endTime;
 
-  DoctorEntity({required this.id, required this.name, required this.specialty, required this.imageUrl, required this.startTime, required this.endTime});
-
+  DoctorEntity(
+      {required this.id,
+      required this.name,
+      required this.specialty,
+      required this.imageUrl,
+      required this.startTime,
+      required this.endTime});
 }
---
+++ lib/feature/main/domain/entites/doctor_entity.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'doctor_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DoctorEntityAdapter extends TypeAdapter<DoctorEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  DoctorEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return DoctorEntity(
+      id: fields[0] as int?,
+      name: fields[1] as String?,
+      specialty: fields[2] as String?,
+      imageUrl: fields[3] as String?,
+      startTime: fields[4] as String?,
+      endTime: fields[5] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, DoctorEntity obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.specialty)
+      ..writeByte(3)
+      ..write(obj.imageUrl)
+      ..writeByte(4)
+      ..write(obj.startTime)
+      ..writeByte(5)
+      ..write(obj.endTime);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DoctorEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
